1f217c8254ea971c11b1c4ec9bc490c88f78f3e4
JoseHenao1 <jose.henao1@udea.edu.co>
2024-10-01 23:37:35 -0500

    Primer commit

---
+++ src/main/java/com/udea/lab2virutal/Lab2virutalApplication.java
@@ -0,0 +1,13 @@
+package com.udea.lab2virutal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab2virutalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab2virutalApplication.class, args);
+	}
+
+}
